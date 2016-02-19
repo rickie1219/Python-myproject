b75e0cf884eb6b1315fa7f82d087a902bb1de042
zhkuo <201221020364@std.uestc.edu.cn>
2016-02-19 14:44:00 +0800

    add ascii.py
    ascii.py:将图片转换为字符画

---
+++ ascii.py
@@ -0,0 +1,62 @@
+"""
+@file ascii.py
+@brief 图片转为字符画
+@author zhangk
+@data 2016-01-22
+"""
+
+from PIL import Image
+import argparse
+
+# 命令行输入参数处理
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file')  # 输入文件
+parser.add_argument('-o', '--output')  # 输出文件
+
+parser.add_argument('--width', type=int, default=80)  # 输出字符画宽
+parser.add_argument('--height', type=int, default=80)  # 输出字符画高
+
+# 获取参数
+args = parser.parse_args()
+
+IMG = args.file
+WIDTH = args.width
+HEIGHT = args.height
+OUTPUT = args.output
+
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+
+
+# 将256灰度映射到70个字符上
+def get_char(r, b, g, alpha=256):
+    if alpha == 0:
+        return ' '
+
+    length = len(ascii_char)
+    gray = int(0.299 * r + 0.587 * g + 0.114 * b)
+
+    unit = (256.0 + 1) / length
+    return ascii_char[int(gray / unit)]
+
+if __name__ == '__main__':
+
+    im = Image.open(IMG)
+    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
+
+    txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j, i)))
+        txt += '\n'
+
+    print(txt)
+
+    # 把字符输出到文本
+    if OUTPUT:
+        with open(OUTPUT, 'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt", 'w') as f:
+            f.write(txt)
